425151f1558ea34101833c7cd4f7774a805e5578
Dylan Tientcheu <dylanmambou17@gmail.com>
2018-02-15 18:16:36 +0100

    :sparkles: add map to app

---
+++ src/Components/GmapComponent/MapContainer.js
@@ -0,0 +1,25 @@
+
+  import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+  import React, { Component } from 'react';
+
+  export class MapContainer extends Component {
+  render() {
+      return (
+        <Map google={this.props.google} zoom={14}>
+  
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+  
+          <InfoWindow onClose={this.onInfoWindowClose}>
+              <div>
+                {/*<h1>{this.state.selectedPlace.name}</h1>*/}
+              </div>
+          </InfoWindow>
+        </Map>
+      );
+    }
+  }
+  
+  export default GoogleApiWrapper({
+    apiKey: ("AIzaSyCD7Cv9p0lYKiJ0pSxKPmxPU2G_zS-FX2Y")
+  })(MapContainer)
