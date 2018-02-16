cdaac233846bcb7af3439ed68db0e2f2da0f3d0c
Dylan Tientcheu <dylanmambou17@gmail.com>
2018-02-16 10:04:28 +0100

    :bug: fix webpack case sensitive issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import MapContainer from "./Components/GmapComponent/MapContainer";
+import MapContainer from "./components/GmapComponent/MapContainer";
 
 class App extends Component {
   render() {
